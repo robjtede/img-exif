a0ddfb43764debfb8dcb491f58c2d35661a9cfd0
Rob Ede <robjtede@icloud.com>
2017-04-19 04:11:12 +0100

    rework to use CSS transforms
    closes #8

--- img-exif.js
+++ img-exif.js
@@ -8,7 +8,7 @@ class ImageExif extends HTMLElement {
   constructor () {
     super()
 
-    this.debug = true
+    this.debug = false
     if (this.debug) console.log('constructing')
 
     this.attachShadow({mode: 'open'})
@@ -20,88 +20,65 @@ class ImageExif extends HTMLElement {
   connectedCallback () {
     if (this.debug) console.log('connected img-exif')
 
-    if (this.src) this.render()
+    this.$figure = this.root.querySelector('figure')
+    this.$img = this.root.querySelector('img')
+
+    this.render()
   }
 
   render () {
-    if (!this.isConnected) return
+    if (this.src && !this.isConnected) return
     if (this.debug) console.log('rendering img-exif')
 
-    // create canvas for image
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')
-
     const $this = this
+    const $img = $this.$img
+    const $figure = $this.$figure
 
     // parse image for orientation
-    EXIF.getData({src: this.src}, function (imgBuffer) {
-      if ($this.debug) console.log('got EXIF data', this.src, imgBuffer)
+    EXIF.getData({src: this.src}, function (buffer) {
+      if ($this.debug) console.log('got EXIF data', this.src)
 
       // get orientation
       const orientation = EXIF.getTag(this, 'Orientation')
-
       if ($this.debug) console.log(`orientation code: ${orientation}`)
 
-      // convert buffer to blob
-      const arrayBufferView = new Uint8Array(imgBuffer)
-      const blob = new Blob([arrayBufferView], {type: 'image/jpeg'})
+      // set image attributes
+      if ($this.alt) $img.alt = $this.alt
 
-      // create blob url
-      const urlCreator = window.URL || window.webkitURL
-      const imageUrl = urlCreator.createObjectURL(blob)
+      // use image data buffer as image src
+      $img.src = URL.createObjectURL(new Blob([buffer]))
+      $img.onload = () => {
+        if ($this.debug) console.log('image loaded')
 
-      // create virtual Image with blob src
-      const img = new Image()
-      img.src = imageUrl
-
-      img.onload = () => {
-        if ($this.debug) console.log('blob image loaded')
-
-        // if image dimensions have changed
-        if ([5, 6, 7, 8].includes(orientation)) {
-          canvas.width = img.height
-          canvas.height = img.width
-        } else {
-          canvas.width = img.width
-          canvas.height = img.height
-        }
-
-        // possible orientation effects
-        const orientations = {
-          1: () => ctx.transform(1, 0, 0, 1, 0, 0),
-          2: () => ctx.transform(-1, 0, 0, 1, img.width, 0),
-          3: () => ctx.transform(-1, 0, 0, -1, img.width, img.height),
-          4: () => ctx.transform(1, 0, 0, -1, 0, img.height),
-          5: () => ctx.transform(0, 1, 1, 0, 0, 0),
-          6: () => ctx.transform(0, 1, -1, 0, img.height, 0),
-          7: () => ctx.transform(0, -1, -1, 0, img.height, img.width),
-          8: () => ctx.transform(0, -1, 1, 0, 0, img.width)
-        }
-
-        // apply orientation to canvas
-        orientations[orientation || 1]()
-
-        // draw image to canvas
-        ctx.drawImage(img, 0, 0)
-
-        // create new image element to be inserted
-        const $img = $this.root.querySelector('img')
-
-        // get blob of canvas
-        canvas.toBlob(blob => {
-          if ($this.debug) console.log('canvas blobified')
-
-          const blobUrl = URL.createObjectURL(blob)
-
-          $this.blobUrl = blobUrl
-
-          // output image
-          $img.src = blobUrl
+        if (!orientation || orientation === 1) {
           if ($this.width) $img.width = $this.width
           if ($this.height) $img.height = $this.height
-          if ($this.alt) $img.alt = $this.alt
-          $this.root.appendChild($img)
-        }, 'image/jpeg')
+          $figure.style.width = `${$img.width}px`
+          $figure.style.height = `${$img.height}px`
+        }
+
+        $img.classList.remove('loading')
+        $img.classList.add(`o${orientation}`)
+
+        if (orientation && orientation !== 1) {
+        // if image dimensions have changed
+          if ([5, 6, 7, 8].includes(orientation)) {
+            if ($this.width) $img.height = $this.width
+            if ($this.height) $img.width = $this.height
+
+            $figure.style.height = `${$img.width}px`
+            $figure.style.width = `${$img.height}px`
+          } else {
+            if ($this.height) $img.height = $this.height
+            if ($this.width) $img.width = $this.height
+
+            $figure.style.width = `${$img.width}px`
+            $figure.style.height = `${$img.height}px`
+          }
+
+          const m = $img.width / 8
+          $this.style.setProperty('--magic', m)
+        }
       }
     })
   }
